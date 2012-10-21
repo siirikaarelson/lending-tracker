81d9e3ccda6008a51d66858b4d91b2bb7245904d
Taavi Sildeberg <taavi@sildeberg.eu>
2012-10-21 22:00:43 +0300

    Added classlibrary and removed enity framework

---
+++ LendingTrackerLibrary/Lender.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LendingTrackerLibrary
+{
+    public class Lender
+    {
+        private string _name;
+        private Gender _gender;
+
+        public Gender Gender
+        {
+            get { return _gender; }
+            set { _gender = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public Lender()
+        {
+        }
+       
+    }
+}
